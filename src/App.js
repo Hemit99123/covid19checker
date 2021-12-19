e07ba45df583c620cb912650254d57b7cfcaa143
Hemit Patel <hemitpatel@computer4u.com>
2021-12-19 11:52:36 -0500

    'fistv_1'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, {useState } from 'react';
 import './App.css';
-
+import moment from 'moment';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    let now = moment().format('MMMM Do YYYY, h:mm a')
+    const [results, setResults] = useState('')
+    const saveData = () => {
+        localStorage.setItem('status', results)
+        alert('Saved')
+        window.location.reload(true)
+    }
 
+    let result = localStorage.getItem('status')
+
+    let pass = "You pass! " + "Date: " + now;
+    let fail = "You fail " + "Date: " + now;
+   return (
+      <div className="App">
+        <h1>COVID-19 symptoms</h1>
+        <p>Please go through these symptoms and check one box! Press enter to enter the data!</p>
+        <ul>
+            <li>fever</li>
+            <li>cough</li>
+            <li>tiredness</li>
+            <li>loss of taste or smell</li>
+        </ul>
+         <input type="radio" name="results" onClick={() => setResults(pass)}></input> I do not have any of these symptoms
+         <br />
+         <br />
+         <input type="radio" name="results" onClick={() => setResults(fail)}></input> I have one or more of these symptoms
+         <br />
+         <br />
+         <button onClick={saveData}>Enter</button>
+         <h3>{result}</h3>
+         
+      </div>
+   );
+}
 export default App;
